b6c24b9d4f5bad5d44b8a0a3260101663eedbf73
Sujithae <sujitha.e.2021.ece@ritchennai.edu.in>
2024-03-20 17:49:53 +0530

    first.js modified file

--- first.js
+++ first.js
@@ -1267,5 +1267,11 @@ console.log(global.message);
 
 console.log(module);
 */
-
+let marks = [34, 45, 98, 26, 51];
+let total = 0;
+for (let i = 0; i <= marks.length; i++) {
+   total += marks[i];
+}
+var no = marks.length;
+console.log(total/no);
 
